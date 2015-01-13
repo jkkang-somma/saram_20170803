0d992b69142a1103045995d09f9f956b6c3eae25
sangheepark <novles@naver.com>
2015-01-13 07:33:28 +0000

    20150113

--- public/js/collection/cm/CommuteCollection.js
+++ public/js/collection/cm/CommuteCollection.js
@@ -13,32 +13,11 @@ define([
         }, 
         save : function(option){
             this.wrapper.saveAll(option);
-        },
-        filterID : function(id){
-            return _.filter(this.models, function(model){
-               if(model.get("id") == id){
-                   return model;
-               } 
-            });
-        },
-        filterDate : function(charDate){
-            return _.filter(this.models, function(model){
-                
-                var startDate = new Date(charDate + " " + "06:00:00");
-                var endDate = new Date(charDate + " " + "06:00:00");
-                endDate.setDate(endDate.getDate() + 1);
-                
-                var modelDate = new Date(model.get("char_date"));
-                
-                if(modelDate > startDate && modelDate <= endDate){
-                    return model;
-                }
-            
-            });
         }
     });
+    
     var CommuteCollectionWrapper = Backbone.Model.extend({
-        url:'/rawdata/bulk',
+        url:'/commute/bulk',
         initialize : function(collection){
             this.collection = collection;
         },
@@ -48,7 +27,6 @@ define([
             this.save("data", data, option);
         }
     });
-    
     
     return CommuteCollection;
 });
