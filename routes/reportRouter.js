1d6d13a1bf74fc0f9ebcf63256b788e3a07e594f
Royleej9 <royleej9@gmail.com>
2015-01-28 21:07:53 +0900

    엑셀 다운로드 추가

--- routes/reportRouter.js
+++ routes/reportRouter.js
@@ -2,25 +2,33 @@ var express = require('express');
 var debug = require('debug')('vacationRouter');
 var router = express.Router();
 var Report = require('../service/Report.js');
-var sessionManager = require('../lib/sessionManager');
+var fs = require('fs');
 var path = require('path');
 var	tempPath = path.normalize(__dirname + '/../temp/');
 
 router.route('/commuteYearReport')
 .get(function(req, res, next){
-	//var tempDir = "../temp/";
-	var fileName = "sample.xlsx";
-	var fileFullPath = tempPath+fileName;
-	console.log(fileFullPath);
-	
-	res.download(fileFullPath);
+		
+	var fileDir = "./excel/files/";
 			
-			
-//	Report.getCommuteYearReport(req.query).then(function(result) {
-//		return res.send(result);
-//	}).catch(function(err) {
-//		next(err);
-//	});
+	Report.getCommuteYearReport(req.query).then(function(fileName) {
+		var filePullPath = fileDir + fileName;
+		res.download(filePullPath, function(err) {
+			if (err) {
+				console.log("excel download fail");
+				next(err);
+			} else {				
+				fs.unlink(filePullPath, function (err) {
+				  if (err)  throw next(err);
+				  
+				  console.log('successfully deleted ' + fileName);
+				});
+				
+			}
+		});
+	}).catch(function(err) {
+		next(err);
+	});
 	
 });
 
