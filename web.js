ea878c1967bd15164988d974290eb620017030eb
novles <novles@naver.com>
2015-02-13 16:04:16 +0900

    cafe24_test

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var batch = require('./batch/batch');
 
 //app.set('port', process.env.PORT);
 // app.set('port', 80);
- app.set('port', 8001);
+ app.set('port', 8002);
 var server = app.listen(app.get('port'), function() {
     debug("==================================================================================================================");
     debug("================================================ Welcome to Saram ================================================");
