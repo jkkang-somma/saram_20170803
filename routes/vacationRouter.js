5b8c03914f10f5b4d23e50fb9f5ad6596e2f9494
Royleej9 <royleej9@gmail.com>
2015-02-16 09:38:42 +0900

    테스트 코드 추가

--- routes/vacationRouter.js
+++ routes/vacationRouter.js
@@ -7,6 +7,10 @@ var sessionManager = require('../lib/sessionManager');
 router.route('/')
 .get(function(req, res, next){
 	Vacation.getVacation(req.query).then(function(result) {
+		for (idx in result) {
+			console.log(result[idx]);
+		}
+		
 		return res.send(result);
 	}).catch(function(err) {
 		next(err);
