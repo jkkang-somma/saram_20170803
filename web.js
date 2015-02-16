44ee990fd5ae7beac37f4c2024c673d356ff5fc7
carran <carran@yescnc.co.kr>
2015-02-16 04:40:21 +0000

    batch

--- web.js
+++ web.js
@@ -18,7 +18,7 @@ var server = app.listen(app.get('port'), function() {
     
     //batchJob
     debug("StartBatchJob");
-    schedule.scheduleJob('43 4 * * *', function(){
+    schedule.scheduleJob('* * * * *', function(){
         batch.backupDb();
     });
 });
