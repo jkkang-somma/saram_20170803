ff3605c7d397c2babf4332ec64604dab2c72b8bc
carran <carran@yescnc.co.kr>
2015-02-26 00:31:18 +0000

    vacation year mod

--- batch/batch.js
+++ batch/batch.js
@@ -55,7 +55,6 @@ var DBBatchJobExecuter = function(){
     for(var idx =0; idx < dbArr.length ; idx++){
         this.result[dbArr[idx]] = {};  
     }
-    console.log(this.result);
 };
 
 DBBatchJobExecuter.prototype.getTables = function(){
@@ -177,48 +176,3 @@ DBBatchJobExecuter.prototype.backupDb = function(){
 };
 
 module.exports = new DBBatchJobExecuter();
-
-// var DBBatchJobExecuter = function(){
-    
-// };
-
-// DBBatchJobExecuter.prototype.backupDb = function(){
-//     var dbInfo = db.getDbInfo();
-//     var filename = moment().format("YYYYMMDD_HHmmss") + ".sql";    
-//     var command = ['mysqldump'];
-    
-//     if(dbInfo.user != "" || _.isNull(dbInfo.user) || _.isUndefined(dbInfo.user)){
-//         command += " -u " + dbInfo.user;
-//     }else {
-//         return;
-//     }
-    
-//     if(dbInfo.password != "" || _.isNull(dbInfo.password) || _.isUndefined(dbInfo.password)){
-//         command += " -p " + dbInfo.password;
-//     }
-
-//     if(dbInfo.host != "" || _.isNull(dbInfo.host) || _.isUndefined(dbInfo.host)){
-//         command += " -h " + dbInfo.host;
-//     }
-    
-//     if(dbInfo.port != "" || _.isNull(dbInfo.port) || _.isUndefined(dbInfo.port)){
-//         command += " -P "+ dbInfo.port;
-//     }
-    
-//     if(dbInfo.database != "" || _.isNull(dbInfo.database) || _.isUndefined(dbInfo.database)){
-//         command += " "+ dbInfo.database;
-//     }
-    
-    
-//     command += " > " + filename;
-    
-//     console.log(command)
-//     exec(command, function(err,out,code){
-//         if(err)
-//             throw err;
-            
-//         process.stderr.write(err);
-//         process.stderr.write(out);
-//         console.log(code);   
-//     });
-// };
