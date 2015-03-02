aa8011a732162462be66f54147b00ac021dc3b47
novles <novles@naver.com>
2015-03-02 16:07:54 +0900

    test

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = require('./app');
 var schedule = require('node-schedule');
 var batch = require('./batch/batch');
 
-app.set('port', 8002);
+app.set('port', 80);
 var server = app.listen(app.get('port'), function() {
     debug("==================================================================================================================");
     debug("================================================ Welcome to Saram ================================================");
