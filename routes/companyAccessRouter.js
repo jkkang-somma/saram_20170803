9224e1271864a564aa4ce1c00e46bbb39eda8b9a
novles <novles@naver.com>
2015-03-13 16:24:24 +0900

    cafe24_test

--- routes/companyAccessRouter.js
+++ routes/companyAccessRouter.js
@@ -8,14 +8,10 @@ var CompanyAccess = require("../service/CompanyAccess.js");
 
 router.route("/")
 .post(function(req,res,next){
-	console.log(req);
 	var session = sessionManager.get(req.cookies.saram);
 	var user = session.user;
 
 	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-	console.log("#############");
-	console.log(req.headers['x-forwarded-for']);
-	console.log(req.connection.remoteAddress);
 
 	var inData = {
 		type : req.body.type,
