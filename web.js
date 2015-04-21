e312f04340359d1de9b172c1e039d2fd394bc284
unknown <goodguy95@yescnc.cafe24app.com>
2015-04-21 15:25:04 +0900

    cafe24_test' '

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = require('./app');
 var schedule = require('node-schedule');
 var batch = require('./batch/batch');
 
-app.set('port', process.env.PORT);
-
+//app.set('port', process.env.PORT);
+app.set('port',8002);
 var server = app.listen(app.get('port'), function() {
     var date = new Date();
     debug(date);
