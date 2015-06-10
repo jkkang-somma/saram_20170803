7afd26dd8bcb6388cd14fe59f5ab93f5d321a5d3
carran <carran@yescnc.co.kr>
2015-06-10 11:13:55 +0000

    CommuteListView Column Title

--- public/js/views/cm/CommuteListView.js
+++ public/js/views/cm/CommuteListView.js
@@ -200,8 +200,9 @@ define([
      	                   		}
      	                   	},
      	                   	{data: "vacation_code", title: i18nCommon.COMMUTE_RESULT_LIST.GRID_COL_NAME.VACATION,
-		                        "render": function (data, type, rowData, meta) {
-		                            return Code.getCodeName(Code.OFFICE, data);
+		                        render: function (data, type, rowData, meta) {
+		                            var codeName = Code.getCodeName(Code.OFFICE, data);
+     	                   			return _.isNull(codeName)? null : codeName.replace(",", "<br>");
 		                       }
 		                    },
      	                   	{ data : "out_office_code", 	"title" : i18nCommon.COMMUTE_RESULT_LIST.GRID_COL_NAME.OUT_OFFICE,
