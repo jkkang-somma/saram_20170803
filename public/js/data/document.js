5fa2f2b6255d8eec467958ca3d883981a30d888b
carran <carran@yescnc.co.kr>
2015-08-20 06:11:52 +0000

    document

---
+++ public/js/data/document.js
@@ -0,0 +1,31 @@
+define([
+    'jquery',
+	'underscore',
+], function($, _){
+	var doc=[
+	    {
+	        value : "물품 구매 요청서",
+	        key : "/doc/2. 물품구매요청서 (수정).xlsx"
+        }, {
+            value : "교통비 청구서",
+	        key : "/doc/6. 교통비청구서(영수증, 월단위).xls"
+        }, {
+            value : "경조금 지급 신청서",
+	        key : "/doc/11. 경조금지급신청서.doc"
+        }, {
+            value : "직원 대출 계약서",
+	        key : "/doc/21. 직원대출계약서.doc"
+        }, {
+            value : "인사 기록 카드",
+	        key : "/doc/23. 인사기록카드.doc"
+        }, {
+            value : "출장비 정산서 신청서",
+	        key : "/doc/25. 출장비 정산서 신청서.xlsx"
+        } 
+    ];
+	return doc;
+
+});
+
+
+
