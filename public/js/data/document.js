608e02d103694c502e9bca955a282bd954b7f935
carran <carran@yescnc.co.kr>
2015-10-30 00:01:23 +0000

    v08

--- public/js/data/document.js
+++ public/js/data/document.js
@@ -4,26 +4,29 @@ define([
 ], function($, _){
 	var doc=[
 	    {
-	        value : "물품 구매 요청서",
-	        key : "/doc/물품구매요청서 (수정).xlsx"
-        }, {
-            value : "교통비 청구서",
-	        key : "/doc/교통비청구서(영수증, 월단위).xls"
-        }, {
-            value : "경조금 지급 신청서",
+	        value : "경조금 지급 신청서",
 	        key : "/doc/경조금지급신청서.doc"
         }, {
-            value : "직원 대출 계약서",
-	        key : "/doc/직원대출계약서.doc"
+            value : "교통비 청구서 (영수증)",
+	        key : "/doc/교통비청구서(영수증).xlsx"
+        }, {
+            value : "교통비 청구서 (월단위)",
+	        key : "/doc/교통비청구서(월단위).xlsx"
+        }, {
+            value : "도서 구입 신청서",
+	        key : "/doc/도서구입신청서.xlsx"
+        }, {
+            value : "물품 구매 요청서",
+	        key : "/doc/물품구매요청서.xlsx"
         }, {
             value : "인사 기록 카드",
 	        key : "/doc/인사기록카드.doc"
         }, {
-            value : "출장비 정산서 신청서",
-	        key : "/doc/출장비 정산서 신청서.xlsx"
+            value : "직원 대출 계약서",
+	        key : "/doc/직원대출계약서.doc"
         }, {
-            value : "도서 구매 신청서",
-	        key : "/doc/도서구매신청서.xlsx"
+            value : "출장비 정산 신청서",
+	        key : "/doc/출장비 정산 신청서.xlsx"
         } 
     ];
 	return doc;
