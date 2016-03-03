11fd483e76d8c470a4111c72ce88c286fc226cf6
carran <carran@yescnc.co.kr>
2016-03-03 01:02:12 +0000

    gt lt

--- routes/inofficeRouter.js
+++ routes/inofficeRouter.js
@@ -7,29 +7,32 @@ var router = express.Router();
 var InOffice = require('../service/InOffice.js');
 
 router.route('/')
-.get(function(req, res) {
-    // var inOffice = new InOffice();
-    InOffice.getInOfficeList(req.query).then(function(result){
-       res.send(result); 
+    .get(function(req, res) {
+        // var inOffice = new InOffice();
+        InOffice.getInOfficeList(req.query).then(function(result) {
+            res.send(result);
+        });
+
     });
-    
-})
 
 router.route('/bulk')
-.put(function(req, res){
-    InOffice.remove(req.body).then(function(){
-        debug("Complete Delete User.");
-        res.send({success:true, message:"Complete Delete User."});
-    }).catch(function(e){
-        debug("Error Delete User.");
-        debug(e);
-        res.status(500);
-        res.send({
-            success:false,
-            message: e.message,
-            error:e
+    .put(function(req, res) {
+        InOffice.remove(req.body).then(function() {
+            debug("Complete Delete User.");
+            res.send({
+                success: true,
+                message: "Complete Delete User."
+            });
+        }).catch(function(e) {
+            debug("Error Delete User.");
+            debug(e);
+            res.status(500);
+            res.send({
+                success: false,
+                message: e.message,
+                error: e
+            });
         });
     });
-});;
 
 module.exports = router;
