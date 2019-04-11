e1b3f7b39a30882e44de0bb2fd484864af741e79
goodguy <goodguy@goodguy-PC>
2019-04-11 09:08:00 +0900

    코멘트 링크 동작 오류 수정

--- public/js/views/dashboard/DashBoardView.js
+++ public/js/views/dashboard/DashBoardView.js
@@ -263,7 +263,7 @@ define([
 					$('.btn-group#privilege-btn-comment').append(appendDiv).after(function(){
 						$(this).click(function(){
 							var $this = $(this);
-							window.location.href = "#commutemanager/comment/";
+							window.location.href = "#commutemanager/comment";
 						});
 					});
 				}
