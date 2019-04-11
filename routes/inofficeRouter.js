c26995143c2ed4bc406eb28ea5d2dece461e0968
goodguy <goodguy@goodguy-PC>
2019-04-11 11:42:25 +0900

    대시보드 로딩 성능 향상 ( 네트워크 전달량 줄임 )

--- routes/inofficeRouter.js
+++ routes/inofficeRouter.js
@@ -5,10 +5,18 @@ var debug = require('debug')('InOfficeRouter');
 var Promise = require('bluebird');
 var router = express.Router();
 var InOffice = require('../service/InOffice.js');
+var sessionManager = require('../lib/sessionManager');
 
 router.route('/')
     .get(function(req, res) {
-        // var inOffice = new InOffice();
+        // 관리자이고, 개인 정보만 조회할 경우
+        // 관리자이고, 모든 정보를 조회할 경우
+        // 관라자가 아닌 경우 ( 무조건 개인정보만 조회 )
+        var adminString=sessionManager.getAdminString(req.cookies.saram);
+        if (adminString !== '%') {
+            // 일반 사용자
+            req.query.id = adminString
+        }
         InOffice.getInOfficeList(req.query).then(function(result) {
             res.send(result);
         });
