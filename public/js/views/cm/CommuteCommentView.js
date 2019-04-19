36d7dc1ec7f4f777dcc8fc0c4b6da4c11b39ee83
goodguy <goodguy@goodguy-PC>
2019-04-19 14:40:46 +0900

    팀장 등급인 경우 대시보드에서 결재, 코멘트 건수 버튼 클릭시 화면 이동 후 상신건이 조회되지 않는 오류 수정

--- public/js/views/cm/CommuteCommentView.js
+++ public/js/views/cm/CommuteCommentView.js
@@ -248,7 +248,7 @@ define([
 			};
 			
 			if (SessionModel.getUserInfo().admin >= Schemas.DEPT_BOSS) {
-				this.gridOption.buttons.push({type:"myRecord", name: "myRecord", filterColumn:["name"], tooltip: ""})
+				this.gridOption.buttons.push({type:"myRecord", name: "myRecord", filterColumn:["name", "approval_name"], tooltip: ""})
 			}
     		this.buttonInit();
 		},
