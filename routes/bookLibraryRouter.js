cac679440f9849b0bfcc7c6ca46cf9ed092a1551
jkkang <jkkang@yescnc.co.kr>
2019-07-23 12:18:04 +0900

    indent 맞춤

--- routes/bookLibraryRouter.js
+++ routes/bookLibraryRouter.js
@@ -7,132 +7,132 @@ var sessionManager = require('../lib/sessionManager');
 
 //라이브러리 조회
 router.route('/library')
-    .get(function(req, res) {
-        BookLibrary.getBookLibrary().then(function(result) {
-            debug("Complete Select Book Library");
-            res.send(result);
-        }).catch(function(e) {
-            debug("Error Select Book Library");
-            res.status(500);
-            res.send({
-                success: false,
-                message: e.message,
-                error: e
-            });
-        });
-    }).post(function(req, res) { //등록
-        var data = req.body;
-        BookLibrary.insertBookLibrary(data).then(function(result) {
-            debug("isnert book success");
-            res.send(result);
-        }).catch(function(e) {
-            debug(e);
-        });
-    }).put(function(req, res) { //삭제
-        var data = req.body;
-        console.log(data);
-        BookLibrary.deleteBookLibrary(data).then(function(result) {
-            debug("delete book success");
-            res.send(result);
-        }).catch(function(e) {
-            debug(e);
-        });
+  .get(function (req, res) {
+    BookLibrary.getBookLibrary().then(function (result) {
+      debug("Complete Select Book Library");
+      res.send(result);
+    }).catch(function (e) {
+      debug("Error Select Book Library");
+      res.status(500);
+      res.send({
+        success: false,
+        message: e.message,
+        error: e
+      });
     });
+  }).post(function (req, res) { //등록
+    var data = req.body;
+    BookLibrary.insertBookLibrary(data).then(function (result) {
+      debug("isnert book success");
+      res.send(result);
+    }).catch(function (e) {
+      debug(e);
+    });
+  }).put(function (req, res) { //삭제
+    var data = req.body;
+    console.log(data);
+    BookLibrary.deleteBookLibrary(data).then(function (result) {
+      debug("delete book success");
+      res.send(result);
+    }).catch(function (e) {
+      debug(e);
+    });
+  });
 
 //이력 조회
 router.route('/history')
-    .get(function(req, res) {
-        var data = req.query;
-        BookLibrary.selectRentHistory(data).then(function(result) {
-            debug("Complete Select Rent history");
-            res.send(result);
-        }).catch(function(e) {
-            debug("Error Select Rent history");
-            res.status(500);
-            res.send({
-                success: false,
-                message: e.message,
-                error: e
-            });
-        });
+  .get(function (req, res) {
+    var data = req.query;
+    BookLibrary.selectRentHistory(data).then(function (result) {
+      debug("Complete Select Rent history");
+      res.send(result);
+    }).catch(function (e) {
+      debug("Error Select Rent history");
+      res.status(500);
+      res.send({
+        success: false,
+        message: e.message,
+        error: e
+      });
     });
+  });
 
 //대출, 반납
 router.route('/rent')
-    .get(function(req, res) {
+  .get(function (req, res) {
 
-    }).post(function(req, res) { //대출
-        var session = sessionManager.get(req.cookies.saram);
-        var data = req.body;
-        data.user_id = session.user.id; // SessionUser
+  }).post(function (req, res) { //대출
+    var session = sessionManager.get(req.cookies.saram);
+    var data = req.body;
+    data.user_id = session.user.id; // SessionUser
 
-        BookLibrary.insertRentBook(data).then(function(result) {
-            debug("insert rent success");
-            BookLibrary.insertRentHistory(data).then(function(result) {
-                debug("insert history suecces");
-                res.send(result);
-            }).catch(function(err) {
-                debug(err);
-            });
-        }).catch(function(e) {
-            debug(e);
-        });
-
-    }).put(function(req, res) { //반납
-        var session = sessionManager.get(req.cookies.saram);
-        var data = req.body;
-        data.user_id = session.user.id;
-        
-        console.log(data);
-
-        BookLibrary.deleteRentBook(data).then(function(result) {
-            debug("return book success");
-            BookLibrary.insertRentHistory(data).then(function(result) {
-                debug("insert history success");
-                res.send(result);
-            }).catch(function(err) {
-                debug(err);
-            });
-        }).catch(function(e) {
-            debug(e);
-        });
+    BookLibrary.insertRentBook(data).then(function (result) {
+      debug("insert rent success");
+      BookLibrary.insertRentHistory(data).then(function (result) {
+        debug("insert history suecces");
+        res.send(result);
+      }).catch(function (err) {
+        debug(err);
+      });
+    }).catch(function (e) {
+      debug(e);
     });
+
+  }).put(function (req, res) { //반납
+    var session = sessionManager.get(req.cookies.saram);
+    var data = req.body;
+    data.user_id = session.user.id;
+
+    console.log(data);
+
+    BookLibrary.deleteRentBook(data).then(function (result) {
+      debug("return book success");
+      BookLibrary.insertRentHistory(data).then(function (result) {
+        debug("insert history success");
+        res.send(result);
+      }).catch(function (err) {
+        debug(err);
+      });
+    }).catch(function (e) {
+      debug(e);
+    });
+  });
 
 
 //naver 조회
 router.route('/regist')
-    .get(function(req, res) {
-        var data = req.query;
-        BookLibrary.getRegistSearch(data).then(function(result) {
-            debug("Complete Regist search");
-            res.send(result);
-        }).catch(function(e) {
-            debug("Error Select Regist search");
-            res.status(500);
-            res.send({
-                success: false,
-                message: e.message,
-                error: e
-            });
-        });
+  .get(function (req, res) {
+    var data = req.query;
+    BookLibrary.getRegistSearch(data).then(function (result) {
+      debug("Complete Regist search");
+      res.send(result);
+    }).catch(function (e) {
+      debug("Error Select Regist search");
+      res.status(500);
+      res.send({
+        success: false,
+        message: e.message,
+        error: e
+      });
     });
+  });
 
 //등록시 ManageNo 조회
 router.route('/manageno')
-    .get(function(req, res) {
-        var data = req.query;
-        BookLibrary.getManageNo(data).then(function(result) {
-            debug("Complete Get Manage Number");
-            res.send(result);
-        }).catch(function(e) {
-            debug("Error Get Manage Number");
-            res.status(500);
-            res.send({
-                success: false,
-                message: e.message,
-                error: e
-            });
-        })
+  .get(function (req, res) {
+    var data = req.query;
+    BookLibrary.getManageNo(data).then(function (result) {
+      debug("Complete Get Manage Number");
+      res.send(result);
+    }).catch(function (e) {
+      debug("Error Get Manage Number");
+      res.status(500);
+      res.send({
+        success: false,
+        message: e.message,
+        error: e
+      });
     })
+  })
 
 module.exports = router;
--- service/BookLibrary.js
+++ service/BookLibrary.js
@@ -8,187 +8,187 @@ var https = require('https');
 
 //검색어를 <b>로 처리하고 있어 삭제
 //제목과 별개로 부제목은 () 안에 들어감 부제목은 삭제
-var replaceAll = function(value) {
-    //값이 없을때 {}로 넘어옴.
-    if (typeof value == 'object')
-        return "";
+var replaceAll = function (value) {
+  //값이 없을때 {}로 넘어옴.
+  if (typeof value == 'object')
+    return "";
 
-    return value.replace(/<b>/gi, "").replace(/<\/b>/gi, "").replace(/\(.*\)/gi, "");
+  return value.replace(/<b>/gi, "").replace(/<\/b>/gi, "").replace(/\(.*\)/gi, "");
 };
 
 //저자가 여러명인 책들 첫번재 제외 다 자름.
-var authorCut = function(value) {
-    var authors = value.split("|");
-    if (authors.length > 0)
-        return authors[0];
-    else
-        return value;
+var authorCut = function (value) {
+  var authors = value.split("|");
+  if (authors.length > 0)
+    return authors[0];
+  else
+    return value;
 }
 
-var imgCut = function(value) {
-    if (typeof value == 'object')
-        return "";
-        
-    var imgStr = value.split("?");
-    if (imgStr.length > 0)
-        return imgStr[0];
-    else
-        return value;
+var imgCut = function (value) {
+  if (typeof value == 'object')
+    return "";
+
+  var imgStr = value.split("?");
+  if (imgStr.length > 0)
+    return imgStr[0];
+  else
+    return value;
 }
 
-var BookLibrary = function() {
-    var _getBookLibrary = function() {
-        return BookLibraryDao.selectBookLibrary();
-    };
+var BookLibrary = function () {
+  var _getBookLibrary = function () {
+    return BookLibraryDao.selectBookLibrary();
+  };
 
-    var _deleteBookLibrary = function(data) {
-        return BookLibraryDao.deleteBookLibrary(data);
-    }
+  var _deleteBookLibrary = function (data) {
+    return BookLibraryDao.deleteBookLibrary(data);
+  }
 
-    var _insertBookLibrary = function(data) {
-        return BookLibraryDao.insertBookLibrary(data);
-    }
+  var _insertBookLibrary = function (data) {
+    return BookLibraryDao.insertBookLibrary(data);
+  }
 
-    var _insertRentBook = function(data) {
-        return BookLibraryDao.insertRentBook(data);
-    }
+  var _insertRentBook = function (data) {
+    return BookLibraryDao.insertRentBook(data);
+  }
 
-    var _deleteRentBook = function(data) {
-        return BookLibraryDao.deleteRentBook(data);
-    }
+  var _deleteRentBook = function (data) {
+    return BookLibraryDao.deleteRentBook(data);
+  }
 
-    var _insertRentHistory = function(data) {
-        return BookLibraryDao.insertRentHistory(data);
-    }
+  var _insertRentHistory = function (data) {
+    return BookLibraryDao.insertRentHistory(data);
+  }
 
-    var _selectRentHistory = function(data) {
-        return BookLibraryDao.selectRentHistory(data);
-    }
+  var _selectRentHistory = function (data) {
+    return BookLibraryDao.selectRentHistory(data);
+  }
 
-    var _getRegistSearch = function(data) {
-        return new Promise(function(resolve, reject) {
+  var _getRegistSearch = function (data) {
+    return new Promise(function (resolve, reject) {
 
-            if (data.searchOpt == undefined || data.searchTxt == undefined)
-                reject();
+      if (data.searchOpt == undefined || data.searchTxt == undefined)
+        reject();
 
-            var url = "/v1/search/book_adv.xml?" + data.searchOpt + "=" + data.searchTxt + "&display=50";
-            url = encodeURI(url);
+      var url = "/v1/search/book_adv.xml?" + data.searchOpt + "=" + data.searchTxt + "&display=50";
+      url = encodeURI(url);
 
-            var options = {
-                hostname: 'openapi.naver.com',
-                path: url,
-                method: 'GET',
-                headers: {
-                    'User-Agent': 'curl/7.49.1',
-                    'Accept': '*/*',
-                    'X-Naver-Client-Id': 'gzDvG8ypz8FoBa27OGHP',
-                    'X-Naver-Client-Secret': 'indsAW7t2X'
-                }
-            };
+      var options = {
+        hostname: 'openapi.naver.com',
+        path: url,
+        method: 'GET',
+        headers: {
+          'User-Agent': 'curl/7.49.1',
+          'Accept': '*/*',
+          'X-Naver-Client-Id': 'gzDvG8ypz8FoBa27OGHP',
+          'X-Naver-Client-Secret': 'indsAW7t2X'
+        }
+      };
 
-            var req = https.request(options, function(res) {
-                var bd = [];
+      var req = https.request(options, function (res) {
+        var bd = [];
 
-                res.on('data', function(d) {
-                    bd.push(d);
-                });
-
-                res.on('end', function() {
-                    digester.digest(Buffer.concat(bd).toString(), function(err, result) {
-                        if (err) {
-                            console.log(err);
-                            reject();
-                        }
-                        else {
-                            var total = result.rss.channel.total;
-                            var items = result.rss.channel.item;
-                            var newItems = [];
-
-                            if (total == 1) {
-                                var obj = {
-                                    'book_name': replaceAll(items.title),
-                                    'img_src': imgCut(items.image),
-                                    'author': authorCut(replaceAll(items.author)),
-                                    'publisher': replaceAll(items.publisher),
-                                    'publishing_date': items.pubdate,
-                                    'isbn': items.isbn,
-                                    'price': items.price,
-                                    'manage_no': ''
-                                };
-                                newItems.push(obj);
-                            }
-                            else if (total > 1) {
-                                _.each(items, function(item) {
-                                    var obj = {
-                                        'book_name': replaceAll(item.title),
-                                        'img_src': imgCut(item.image),
-                                        'author': authorCut(replaceAll(item.author)),
-                                        'publisher': replaceAll(item.publisher),
-                                        'publishing_date': item.pubdate,
-                                        'isbn': item.isbn,
-                                        'price': item.price,
-                                        'manage_no': ''
-                                    };
-                                    newItems.push(obj);
-                                });
-                            }
-                            resolve(newItems);
-                        }
-                    })
-                });
-            });
-
-            req.on('error', function(e) {
-                console.error(e);
-            });
-
-            req.end();
+        res.on('data', function (d) {
+          bd.push(d);
         });
-    }
 
-    var _getManageNo = function(data) {
-        return new Promise(function(resolve, reject) {
+        res.on('end', function () {
+          digester.digest(Buffer.concat(bd).toString(), function (err, result) {
+            if (err) {
+              console.log(err);
+              reject();
+            }
+            else {
+              var total = result.rss.channel.total;
+              var items = result.rss.channel.item;
+              var newItems = [];
 
-            BookLibraryDao.selectManageNo(data).then(function(result) {
-
-                var arr = [];
-                _.each(result, function(item) {
-                    arr.push(parseInt(item.manage_no.replace(data.manageno, '')));
+              if (total == 1) {
+                var obj = {
+                  'book_name': replaceAll(items.title),
+                  'img_src': imgCut(items.image),
+                  'author': authorCut(replaceAll(items.author)),
+                  'publisher': replaceAll(items.publisher),
+                  'publishing_date': items.pubdate,
+                  'isbn': items.isbn,
+                  'price': items.price,
+                  'manage_no': ''
+                };
+                newItems.push(obj);
+              }
+              else if (total > 1) {
+                _.each(items, function (item) {
+                  var obj = {
+                    'book_name': replaceAll(item.title),
+                    'img_src': imgCut(item.image),
+                    'author': authorCut(replaceAll(item.author)),
+                    'publisher': replaceAll(item.publisher),
+                    'publishing_date': item.pubdate,
+                    'isbn': item.isbn,
+                    'price': item.price,
+                    'manage_no': ''
+                  };
+                  newItems.push(obj);
                 });
-
-                if (arr.length == 0)
-                    resolve({ manage_no: 1 });
-
-                if (arr.length == 1 && arr[0] != 1)
-                    resolve({ manage_no: 2 });
-
-                var currentNo = 0;
-                for (var i = 1; i < arr.length; i++) {
-                    var previousNo = arr[i - 1];
-                    currentNo = arr[i];
-
-                    if (previousNo == currentNo)
-                        continue;
-
-                    if (previousNo + 1 != currentNo) {
-                        resolve({ manage_no: previousNo + 1 });
-                    }
-                }
-                resolve({ manage_no: currentNo + 1 });
-            });
+              }
+              resolve(newItems);
+            }
+          })
         });
-    }
+      });
 
-    return {
-        getBookLibrary: _getBookLibrary,
-        deleteBookLibrary: _deleteBookLibrary,
-        insertBookLibrary: _insertBookLibrary,
-        insertRentBook: _insertRentBook,
-        deleteRentBook: _deleteRentBook,
-        insertRentHistory: _insertRentHistory,
-        selectRentHistory: _selectRentHistory,
-        getRegistSearch: _getRegistSearch,
-        getManageNo: _getManageNo
-    };
+      req.on('error', function (e) {
+        console.error(e);
+      });
+
+      req.end();
+    });
+  }
+
+  var _getManageNo = function (data) {
+    return new Promise(function (resolve, reject) {
+
+      BookLibraryDao.selectManageNo(data).then(function (result) {
+
+        var arr = [];
+        _.each(result, function (item) {
+          arr.push(parseInt(item.manage_no.replace(data.manageno, '')));
+        });
+
+        if (arr.length == 0)
+          resolve({ manage_no: 1 });
+
+        if (arr.length == 1 && arr[0] != 1)
+          resolve({ manage_no: 2 });
+
+        var currentNo = 0;
+        for (var i = 1; i < arr.length; i++) {
+          var previousNo = arr[i - 1];
+          currentNo = arr[i];
+
+          if (previousNo == currentNo)
+            continue;
+
+          if (previousNo + 1 != currentNo) {
+            resolve({ manage_no: previousNo + 1 });
+          }
+        }
+        resolve({ manage_no: currentNo + 1 });
+      });
+    });
+  }
+
+  return {
+    getBookLibrary: _getBookLibrary,
+    deleteBookLibrary: _deleteBookLibrary,
+    insertBookLibrary: _insertBookLibrary,
+    insertRentBook: _insertRentBook,
+    deleteRentBook: _deleteRentBook,
+    insertRentHistory: _insertRentHistory,
+    selectRentHistory: _selectRentHistory,
+    getRegistSearch: _getRegistSearch,
+    getManageNo: _getManageNo
+  };
 }
 module.exports = new BookLibrary();
