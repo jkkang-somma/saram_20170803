293d068fca654080e91cce77f4e5b6045d50ca9e
jkkang <jkkang@yescnc.co.kr>
2019-08-01 12:18:29 +0900

    indent

--- public/js/views/sm/popup/AddPartView.js
+++ public/js/views/sm/popup/AddPartView.js
@@ -12,153 +12,153 @@ define([
   'collection/common/CodeCollection',
   'datatables',
   'collection/sm/UserCollection',
-], function($, _, Backbone, BaseView, log, Dialog, i18nCommon, Form, Code, PartModel, CodeCollection, Datatables, UserCollection){
-    var LOG= log.getLogger("AddPartView");
-	var autocompleteId = "autocomplete";
-	var availableTagsUser = [];
+], function ($, _, Backbone, BaseView, log, Dialog, i18nCommon, Form, Code, PartModel, CodeCollection, Datatables, UserCollection) {
+  var LOG = log.getLogger("AddPartView");
+  var autocompleteId = "autocomplete";
+  var availableTagsUser = [];
 
-	var AddPartView = BaseView.extend({
-    	initialize:function(){
-    		$(this.el).html('');
-    	    $(this.el).empty();
-    	    
-    	    this.model=new PartModel();
-    	    _.bindAll(this, "submitAdd");
-    	},
-    	render:function(el){
-    	    var dfd= new $.Deferred();
-    	    var _view=this;
-    	    var comboItem = [{key:"", value:" "}];
-    	    if (!_.isUndefined(el)){
-    	        this.el=el;
-    	    }
-    	    
-                var _model=_view.model.attributes;
-        	    var _form = new Form({
-        	        el:_view.el,
-        	        form:undefined,
-        	        childs:[{
-        	                type:"input",
-        	                name:"code",
-        	                label:i18nCommon.PART_LIST.GRID_COL_NAME.CODE,
-        	                value:_model.code,
-        	        },{
-        	                type:"input",
-        	                name:"name",
-        	                label:i18nCommon.PART_LIST.GRID_COL_NAME.NAME,
-        	                value:_model.name,
-        	        },{
-							//type:"input",
-							type:"auto_input",
-        	        		name:"leader",
-        	        		label:i18nCommon.PART_LIST.GRID_COL_NAME.LEADER,
-							value:_model.leader,
-							id:autocompleteId
-        	        },{
-						type:"empty_data",
-						name:"user_name",
-						label:i18nCommon.DEPARTMENT_LIST.GRID_COL_NAME.LEADER,
-						value:_model.leader,
-					}]
-        	    });
-        	    
-        	    _form.render().done(function(){
-        	        _view.form=_form;
-					//dfd.resolve();
-					dfd.resolve(_view);
-        	    }).fail(function(){
-        	        dfd.reject();
-        	    });  
+  var AddPartView = BaseView.extend({
+    initialize: function () {
+      $(this.el).html('');
+      $(this.el).empty();
 
-    	    return dfd.promise();
-     	},
-		afterRender: function(){
-			$(document).ready(function() {
-				var userCollection = new UserCollection();
-				userCollection.fetch({
-					success : function(result){
-						if (result.length > 1) {
-							var userCnt = 0;
-							for(var index = 0; index < result.models.length; index++) {
-								if (_.isEmpty(result.models[index].attributes.leave_company) || result.models[index].attributes.leave_company == null) {
-									availableTagsUser[userCnt] = result.models[index].attributes.name + "(" + result.models[index].attributes.id + ")";
-									//console.log(availableTagsUser[userCnt]);
-									userCnt++;
-								}
-								else {
-									//console.log("Leave_company[" + result.models[index].attributes.name + "][" + result.models[index].attributes.leave_company + "]");
-								}
-							}
-							// for( var index = 0; index < result.models.length; index++) {
-							// 	availableTagsUser[index] = result.models[index].attributes.name + "(" + result.models[index].attributes.id + ")";
-							// 	console.log(availableTagsUser[index]);
-							// }
-						}
-						else {
-							console.log("userCollection data is null!!!");
-						}
-					}
-				})
-				$("#autocomplete").autocomplete({
-					source: availableTagsUser
-				});
-				$(".ui-autocomplete").css("position", "absolute");
-				$(".ui-autocomplete").css("top", "100px");
-				$(".ui-autocomplete").css("left", "100px");
-				$(".ui-autocomplete").css("z-index", "2147483647");
-				$(".ui-autocomplete").css("background", "#FFFFFF");
-			});
-		},    	
-    	submitAdd : function(){
-    	    var view = this;
-    	    var dfd= new $.Deferred();
-			var _view=this,_form=this.form,_data=_form.getData();
-			if(!view.checkFormData(_data.leader)) {
-				Dialog.warning(i18nCommon.IPCONFIRM.IP.INVALID_USER);
-				dfd.reject();
-				return;
-			}
-			var firstArr = (_data.leader).split("(");
-			var strTemp = firstArr[1].split(")");
-			_data.leader = strTemp[0];
-			_data.user_name = firstArr[0];
-			
-			var _partModel=new PartModel(_data);
-            
-    	    _partModel.save({},{
-    	        success:function(model, xhr, options){
-    	    		Code.init().then(
-    	    				function(){
-    	    					dfd.resolve(_.defaults(_data, _partModel.default));
-    	    				});
-    	            //dfd.resolve(_.defaults(_data, _partModel.default));
-    	        },
-    	        error:function(model, xhr, options){
-    	            var respons=xhr.responseJSON;
-    	            dfd.reject();
-    	        },
-    	        wait:false
-    	    });  
-    	    
-    	    dfd.resolve(_data);
-    	    return dfd.promise();
-    	},
-    	checkFormData: function(data) {
-			if(availableTagsUser.indexOf(data) == -1) {
-				return false;
-			}
-			return true;
-		},
-    	getFormData: function(form) {
-    	    var unindexed_array = form.serializeArray();
-    	    var indexed_array= {};
-    	    
-    	    $.map(unindexed_array, function(n, i){
-                indexed_array[n['name']] = n['value'];
+      this.model = new PartModel();
+      _.bindAll(this, "submitAdd");
+    },
+    render: function (el) {
+      var dfd = new $.Deferred();
+      var _view = this;
+      var comboItem = [{ key: "", value: " " }];
+      if (!_.isUndefined(el)) {
+        this.el = el;
+      }
+
+      var _model = _view.model.attributes;
+      var _form = new Form({
+        el: _view.el,
+        form: undefined,
+        childs: [{
+          type: "input",
+          name: "code",
+          label: i18nCommon.PART_LIST.GRID_COL_NAME.CODE,
+          value: _model.code,
+        }, {
+          type: "input",
+          name: "name",
+          label: i18nCommon.PART_LIST.GRID_COL_NAME.NAME,
+          value: _model.name,
+        }, {
+          //type:"input",
+          type: "auto_input",
+          name: "leader",
+          label: i18nCommon.PART_LIST.GRID_COL_NAME.LEADER,
+          value: _model.leader,
+          id: autocompleteId
+        }, {
+          type: "empty_data",
+          name: "user_name",
+          label: i18nCommon.DEPARTMENT_LIST.GRID_COL_NAME.LEADER,
+          value: _model.leader,
+        }]
+      });
+
+      _form.render().done(function () {
+        _view.form = _form;
+        //dfd.resolve();
+        dfd.resolve(_view);
+      }).fail(function () {
+        dfd.reject();
+      });
+
+      return dfd.promise();
+    },
+    afterRender: function () {
+      $(document).ready(function () {
+        var userCollection = new UserCollection();
+        userCollection.fetch({
+          success: function (result) {
+            if (result.length > 1) {
+              var userCnt = 0;
+              for (var index = 0; index < result.models.length; index++) {
+                if (_.isEmpty(result.models[index].attributes.leave_company) || result.models[index].attributes.leave_company == null) {
+                  availableTagsUser[userCnt] = result.models[index].attributes.name + "(" + result.models[index].attributes.id + ")";
+                  //console.log(availableTagsUser[userCnt]);
+                  userCnt++;
+                }
+                else {
+                  //console.log("Leave_company[" + result.models[index].attributes.name + "][" + result.models[index].attributes.leave_company + "]");
+                }
+              }
+              // for( var index = 0; index < result.models.length; index++) {
+              // 	availableTagsUser[index] = result.models[index].attributes.name + "(" + result.models[index].attributes.id + ")";
+              // 	console.log(availableTagsUser[index]);
+              // }
+            }
+            else {
+              console.log("userCollection data is null!!!");
+            }
+          }
+        })
+        $("#autocomplete").autocomplete({
+          source: availableTagsUser
+        });
+        $(".ui-autocomplete").css("position", "absolute");
+        $(".ui-autocomplete").css("top", "100px");
+        $(".ui-autocomplete").css("left", "100px");
+        $(".ui-autocomplete").css("z-index", "2147483647");
+        $(".ui-autocomplete").css("background", "#FFFFFF");
+      });
+    },
+    submitAdd: function () {
+      var view = this;
+      var dfd = new $.Deferred();
+      var _view = this, _form = this.form, _data = _form.getData();
+      if (!view.checkFormData(_data.leader)) {
+        Dialog.warning(i18nCommon.IPCONFIRM.IP.INVALID_USER);
+        dfd.reject();
+        return;
+      }
+      var firstArr = (_data.leader).split("(");
+      var strTemp = firstArr[1].split(")");
+      _data.leader = strTemp[0];
+      _data.user_name = firstArr[0];
+
+      var _partModel = new PartModel(_data);
+
+      _partModel.save({}, {
+        success: function (model, xhr, options) {
+          Code.init().then(
+            function () {
+              dfd.resolve(_.defaults(_data, _partModel.default));
             });
-            
-            return indexed_array;
-    	}
-    });
-    return AddPartView;
+          //dfd.resolve(_.defaults(_data, _partModel.default));
+        },
+        error: function (model, xhr, options) {
+          var respons = xhr.responseJSON;
+          dfd.reject();
+        },
+        wait: false
+      });
+
+      dfd.resolve(_data);
+      return dfd.promise();
+    },
+    checkFormData: function (data) {
+      if (availableTagsUser.indexOf(data) == -1) {
+        return false;
+      }
+      return true;
+    },
+    getFormData: function (form) {
+      var unindexed_array = form.serializeArray();
+      var indexed_array = {};
+
+      $.map(unindexed_array, function (n, i) {
+        indexed_array[n['name']] = n['value'];
+      });
+
+      return indexed_array;
+    }
+  });
+  return AddPartView;
 });
--- public/js/views/sm/popup/EditPartView.js
+++ public/js/views/sm/popup/EditPartView.js
@@ -13,153 +13,153 @@ define([
   'collection/common/CodeCollection',
   'text!templates/default/input.html',
   'collection/sm/UserCollection',
-], function($, _, _S, Backbone, BaseView, log, Dialog, i18nCommon, Form, PartModel, Code, CodeCollection, container, UserCollection){
-    var LOG= log.getLogger("EditPartView");
-	var autocompleteId = "autocomplete";
-	var availableTagsUser = [];
-	var init_user;
+], function ($, _, _S, Backbone, BaseView, log, Dialog, i18nCommon, Form, PartModel, Code, CodeCollection, container, UserCollection) {
+  var LOG = log.getLogger("EditPartView");
+  var autocompleteId = "autocomplete";
+  var availableTagsUser = [];
+  var init_user;
 
-	var EditPartView = BaseView.extend({
-    	initialize:function(data){
-    		$(this.el).html('');
-    	    $(this.el).empty();
-    	    
-    	    this.model=new PartModel(data);
-    	    _.bindAll(this, "submitSave");
-    	},
-    	render:function(el){
-    	    var dfd= new $.Deferred();
-    	    var _view=this;
-    	    var comboItem = [{key:"", value:" "}];
-    	    if (!_.isUndefined(el)){
-    	        this.el=el;
-    	    }
-    	   
-			  var _model=_view.model.attributes;
-			  if(_model.leader == null || _model.user_name == null) {
-				init_user = "";
-			  }
-			  else {
-				init_user = _model.user_name + "(" + _model.leader + ")";
-			  }
-        	    var _form = new Form({
-        	        el:_view.el,
-        	        form:undefined,
-        	        childs:[{
-        	                type:"input",
-        	                name:"code",
-        	                label:i18nCommon.PART_LIST.CODE,
-        	                value:_model.code,
-        	                disabled:"readonly",
-        	        },{
-        	                type:"input",
-        	                name:"name",
-        	                label:i18nCommon.PART_LIST.NAME,
-        	                value:_model.name,
-        	        },{
-							//type:"input",
-							type:"auto_input",
-							name:"leader",
-							id:autocompleteId,
-    	        			label:i18nCommon.PART_LIST.GRID_COL_NAME.LEADER,
-    	        			value:_model.leader,
-        	        },{
-						type:"empty_data",
-						name:"user_name",
-						label:i18nCommon.DEPARTMENT_LIST.GRID_COL_NAME.LEADER,
-						value:_model.leader,
-					}]
-        	    });
-        	    
-        	    _form.render().done(function(){
-        	        _view.form=_form;
-					//dfd.resolve();
-					dfd.resolve(_view);
-        	    }).fail(function(){
-        	        dfd.reject();
-        	    });  
-        	    
-    	    return dfd.promise();
-     	},
-		afterRender: function(){
-			$(document).ready(function() {
-				var userCollection = new UserCollection();
-				userCollection.fetch({
-					success : function(result){
-						if (result.length > 1) {
-							var userCnt = 0;
-							for(var index = 0; index < result.models.length; index++) {
-								if (_.isEmpty(result.models[index].attributes.leave_company) || result.models[index].attributes.leave_company == null) {
-									availableTagsUser[userCnt] = result.models[index].attributes.name + "(" + result.models[index].attributes.id + ")";
-									//console.log(availableTagsUser[userCnt]);
-									userCnt++;
-								}
-								else {
-									//console.log("Leave_company[" + result.models[index].attributes.name + "][" + result.models[index].attributes.leave_company + "]");
-								}
-							}
-							// for( var index = 0; index < result.models.length; index++) {
-							// 	availableTagsUser[index] = result.models[index].attributes.name + "(" + result.models[index].attributes.id + ")";
-							// 	console.log(availableTagsUser[index]);
-							// }
-						}
-						else {
-							console.log("userCollection data is null!!!");
-						}
-					}
-				})
-				$("#autocomplete").autocomplete({
-					source: availableTagsUser
-				});
-				$(".ui-autocomplete").css("position", "absolute");
-				$(".ui-autocomplete").css("top", "100px");
-				$(".ui-autocomplete").css("left", "100px");
-				$(".ui-autocomplete").css("z-index", "2147483647");
-				$(".ui-autocomplete").css("background", "#FFFFFF");
+  var EditPartView = BaseView.extend({
+    initialize: function (data) {
+      $(this.el).html('');
+      $(this.el).empty();
 
-				$('#autocomplete').val(init_user);
-			});
-		},
-		submitSave : function(e){
-    	    var dfd= new $.Deferred();
-			var _view=this,_form=this.form,_data=_form.getData();
+      this.model = new PartModel(data);
+      _.bindAll(this, "submitSave");
+    },
+    render: function (el) {
+      var dfd = new $.Deferred();
+      var _view = this;
+      var comboItem = [{ key: "", value: " " }];
+      if (!_.isUndefined(el)) {
+        this.el = el;
+      }
 
-			if(!_view.checkFormData(_data.leader)) {
-				Dialog.warning(i18nCommon.IPCONFIRM.IP.INVALID_USER);
-				dfd.reject();
-				return;
-			}
+      var _model = _view.model.attributes;
+      if (_model.leader == null || _model.user_name == null) {
+        init_user = "";
+      }
+      else {
+        init_user = _model.user_name + "(" + _model.leader + ")";
+      }
+      var _form = new Form({
+        el: _view.el,
+        form: undefined,
+        childs: [{
+          type: "input",
+          name: "code",
+          label: i18nCommon.PART_LIST.CODE,
+          value: _model.code,
+          disabled: "readonly",
+        }, {
+          type: "input",
+          name: "name",
+          label: i18nCommon.PART_LIST.NAME,
+          value: _model.name,
+        }, {
+          //type:"input",
+          type: "auto_input",
+          name: "leader",
+          id: autocompleteId,
+          label: i18nCommon.PART_LIST.GRID_COL_NAME.LEADER,
+          value: _model.leader,
+        }, {
+          type: "empty_data",
+          name: "user_name",
+          label: i18nCommon.DEPARTMENT_LIST.GRID_COL_NAME.LEADER,
+          value: _model.leader,
+        }]
+      });
 
-			var firstArr = (_data.leader).split("(");
-			var strTemp = firstArr[1].split(")");
-			_data.leader = strTemp[0];
-			_data.user_name = firstArr[0];
-			
-    	    var _partModel= new PartModel(_data);
-    	    _partModel.attributes._code="-2";
-    	    _partModel.save({},{
-    	        success:function(model, xhr, options){
-    	    		Code.init().then(function(){
-    	    			 dfd.resolve(_data);
-    	    		});
-    	            //dfd.resolve(_data);
-    	        },
-    	        error:function(model, xhr, options){
-    	            var respons=xhr.responseJSON;
-    	            Dialog.error(respons.message);
-    	            dfd.reject();
-    	        },
-    	        wait:false
-    	    });
-    	    
-    	    return dfd.promise();
-    	},
-		checkFormData: function(data) {
-			if(availableTagsUser.indexOf(data) == -1) {
-				return false;
-			}
-			return true;
-		},
-    });
-    return EditPartView;
+      _form.render().done(function () {
+        _view.form = _form;
+        //dfd.resolve();
+        dfd.resolve(_view);
+      }).fail(function () {
+        dfd.reject();
+      });
+
+      return dfd.promise();
+    },
+    afterRender: function () {
+      $(document).ready(function () {
+        var userCollection = new UserCollection();
+        userCollection.fetch({
+          success: function (result) {
+            if (result.length > 1) {
+              var userCnt = 0;
+              for (var index = 0; index < result.models.length; index++) {
+                if (_.isEmpty(result.models[index].attributes.leave_company) || result.models[index].attributes.leave_company == null) {
+                  availableTagsUser[userCnt] = result.models[index].attributes.name + "(" + result.models[index].attributes.id + ")";
+                  //console.log(availableTagsUser[userCnt]);
+                  userCnt++;
+                }
+                else {
+                  //console.log("Leave_company[" + result.models[index].attributes.name + "][" + result.models[index].attributes.leave_company + "]");
+                }
+              }
+              // for( var index = 0; index < result.models.length; index++) {
+              // 	availableTagsUser[index] = result.models[index].attributes.name + "(" + result.models[index].attributes.id + ")";
+              // 	console.log(availableTagsUser[index]);
+              // }
+            }
+            else {
+              console.log("userCollection data is null!!!");
+            }
+          }
+        })
+        $("#autocomplete").autocomplete({
+          source: availableTagsUser
+        });
+        $(".ui-autocomplete").css("position", "absolute");
+        $(".ui-autocomplete").css("top", "100px");
+        $(".ui-autocomplete").css("left", "100px");
+        $(".ui-autocomplete").css("z-index", "2147483647");
+        $(".ui-autocomplete").css("background", "#FFFFFF");
+
+        $('#autocomplete').val(init_user);
+      });
+    },
+    submitSave: function (e) {
+      var dfd = new $.Deferred();
+      var _view = this, _form = this.form, _data = _form.getData();
+
+      if (!_view.checkFormData(_data.leader)) {
+        Dialog.warning(i18nCommon.IPCONFIRM.IP.INVALID_USER);
+        dfd.reject();
+        return;
+      }
+
+      var firstArr = (_data.leader).split("(");
+      var strTemp = firstArr[1].split(")");
+      _data.leader = strTemp[0];
+      _data.user_name = firstArr[0];
+
+      var _partModel = new PartModel(_data);
+      _partModel.attributes._code = "-2";
+      _partModel.save({}, {
+        success: function (model, xhr, options) {
+          Code.init().then(function () {
+            dfd.resolve(_data);
+          });
+          //dfd.resolve(_data);
+        },
+        error: function (model, xhr, options) {
+          var respons = xhr.responseJSON;
+          Dialog.error(respons.message);
+          dfd.reject();
+        },
+        wait: false
+      });
+
+      return dfd.promise();
+    },
+    checkFormData: function (data) {
+      if (availableTagsUser.indexOf(data) == -1) {
+        return false;
+      }
+      return true;
+    },
+  });
+  return EditPartView;
 });
